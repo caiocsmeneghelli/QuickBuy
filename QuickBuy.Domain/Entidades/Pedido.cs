1e3cac794e1710c2200379a5dffd8fa0d38edb8f
Caio Cesar <caiocsmeneghelli@outlook.com>
2021-03-01 18:47:40 -0300

    Criação das entidades.

---
+++ QuickBuy.Domain/Entidades/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using QuickBuy.Domain.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public int NumeroEndereco { get; set; }
+        public string EnderecoCompleto { get; set; }
+
+        public int FormaDePagamentoId { get; set; }
+        public FormaDePagamento FormaDePagamento { get; set; }
+
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Domain/ObjetoDeValor/FormaDePagamento.cs
@@ -0,0 +1,19 @@
+﻿using QuickBuy.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.ObjetoDeValor
+{
+    public class FormaDePagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto { get { return Id == (int)TipoFormaPagamentoEnum.Boleto; } }
+        public bool EhCartaoCredito { get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; } }
+        public bool EhDeposito { get { return Id == (int)TipoFormaPagamentoEnum.Deposito; } }
+        public bool NaoFoiDefinido { get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; } }
+
+    }
+}
